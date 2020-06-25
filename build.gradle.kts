e48faf78d68f8a7fadbbf7272035669278098f88
Jade Kim <jinyong@jadekim.kr>
2020-06-25 12:26:23 +0900

    Release v0.2.2
     - Update library version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 val artifactName = "lettuce-extension"
 val artifactGroup = "kr.jadekim"
-val artifactVersion = "0.2.1"
+val artifactVersion = "0.2.2"
 group = artifactGroup
 version = artifactVersion
 
